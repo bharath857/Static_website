dd5147ae57f3d0bfe99a80cfc419174dbe5f30ef
bharath857 <bharath.reddy.857@gmail.com>
2022-05-09 18:53:42 +0530

    initial code for my website

--- MyWebsite/src/app/app.component.spec.ts
+++ MyWebsite/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'resume'`, () => {
+  it(`should have as title 'MyWebsite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('resume');
+    expect(app.title).toEqual('MyWebsite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('resume app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MyWebsite app is running!');
   });
 });
