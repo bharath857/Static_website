7a421eb906f7071c2fd41f4eb019145826a00880
bharath857 <bharath.reddy.857@gmail.com>
2022-08-08 17:15:18 +0530

    resume content updated

--- MyWebsite/src/app/app-routing.module.ts
+++ MyWebsite/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './features/about/about.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { HomeComponent } from './features/home/home.component';
+import { ProjectDetailsComponent } from './features/project-details/project-details.component';
 
 
 const routes: Routes = [
     {path:'', pathMatch:'full', redirectTo:'/home'},
     {path:'home', component:HomeComponent},
     {path:'about', component:AboutComponent},
+    {path:'projects', component:ProjectDetailsComponent},
     {path:'contact', component:ContactComponent}
     
     
